05ccd92207245babd57957dce0b30367401aaa10
unknown <alexandre.delgado@sfr.fr>
2024-04-17 16:12:06 +0200

    création des trois design frontend

--- app/src/main/java/com/example/gf_attendance/MainActivity.java
+++ app/src/main/java/com/example/gf_attendance/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.gf_attendance;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_open;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.launch_screen);
+
+        btn_open = findViewById(R.id.open_LoginPage);
+
+        btn_open.setOnClickListener(v -> {
+            Intent intent =new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+
+        });
     }
 }
